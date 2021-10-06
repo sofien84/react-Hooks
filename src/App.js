14524484b54d6e66f5805b3c2e5caf9c8b1f7685
sofien48 <alyahyaouisofien@gmail.com>
2021-10-06 09:54:44 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react'
+import MovieList from './Components/MovieList'
+import Navbarr from './Components/Navbar/Navbarr'
+import  {MoviesData} from './Components/MoviesData' 
+import AddMovie from './Components/AddMovie/AddMovie'
+import './App.css'
 
-function App() {
+const App = () => {
+  const [Movies, setMovies] = useState(MoviesData)
+  const [name, setName] = useState ("")
+  const [ratingSearch, setRatingSearch] = useState(0)
+  const Addfilm = (newmovie) => {
+
+    return setMovies([...Movies, newmovie])
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbarr name={name} setName={setName}  ratingSearch={ratingSearch}  setRatingSearch={setRatingSearch}/>
+      <div className = "positionADD" style={{alignItems:"center"}}>
+        <span>
+        <MovieList Movies={Movies}  name={name}  ratingSearch={ratingSearch}/>
+        </span>
+        <span>  
+         <AddMovie Addfilm={Addfilm}/>
+        </span>
+      </div>
     </div>
   );
 }
